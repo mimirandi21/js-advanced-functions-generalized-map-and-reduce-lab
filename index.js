a9de266dc181772de1faa0ae206d39b598c8567d
Randi Schreiner <mimirandi21@gmail.com>
2021-01-20 16:21:20 -0600

    commit tree 5976c3b58d7f27a252cda08089ae111dbab9e688

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+let map = (sourceArray, func) => {
+	let arr = [];
+	for (let i = 0; i < sourceArray.length; i++) {
+		let thisOne = sourceArray[i];
+		arr.push(func(thisOne));
+	}
+	return arr;
+};
+
+let reduce = (sourceArray, func, startingPoint = 0) => {
+	let tmp;
+
+	if (startingPoint) {
+		tmp = startingPoint;
+		for (let i = 0; i < sourceArray.length; i++) {
+			tmp = func(sourceArray[i], tmp);
+		}
+	} else {
+		tmp = sourceArray[0];
+		for (let i = 1; i < sourceArray.length; i++) {
+			tmp = func(sourceArray[i], tmp);
+		}
+	}
+	return tmp;
+};
